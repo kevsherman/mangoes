701de546eb45f58677ea9d357ff42e5faaed2656
Kevin Sherman <kevsherman@gmail.com>
2014-06-02 17:38:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mangoes::Application.config.secret_key_base = '39b5ea8831ec74ed8c00476fec0da647b1e60c076da38509da6d9d2f41de46115ce90cd5c7c9374028e19c3f10e4a8d098ff92c4ab62cd6f3a26ee92ee9b4fee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mangoes::Application.config.session_store :cookie_store, key: '_Mangoes_session'
